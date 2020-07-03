8bce12c67362be84571cb8c076fe1009763c59af
Orlando <xlandoxrojasx@gmail.com>
2020-07-03 10:09:44 -0500

    Se integro el sidebar a admin.vue para generar un metodo que permita cambiar las tablas y se agrega la tabla de muestra a Table_u.

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,20 +1,42 @@
 <template>
     <div class="admin">
         <Title msg="Admin"/>
-        <Sidebar></Sidebar>
-        
+        <b-row class="mb-5">
+            <b-col cols="12">
+                <b-button v-b-toggle.sidebar-no-header variant="success">Menú</b-button>
+                <b-sidebar id="sidebar-no-header" aria-labelledby="sidebar-no-header-title" no-header shadow>
+                    <template v-slot:default="{ hide }">
+                        <div class="p-3">
+                            <h3 id="sidebar-no-header-title" class="my-5">Hola Administrador</h3>
+                            <p>Seleccioné la colección perteneciente a la base de datos que desea modificar:</p>
+                            <nav class="mb-5">
+                                <b-nav vertical class="mt-5">
+                                    <b-nav-item @click="hide">Usuarios</b-nav-item>
+                                    <b-nav-item @click="hide">Exponentes</b-nav-item>
+                                    <b-nav-item @click="hide">Cursos</b-nav-item>
+                                    <b-nav-item @click="hide">Blogs</b-nav-item>
+                                    <b-nav-item @click="hide">Noticias</b-nav-item>
+                                </b-nav>
+                            </nav>
+                            <b-button variant="warning" block @click="hide">Cerrar Menú</b-button>
+                        </div>
+                    </template>
+                </b-sidebar>
+            </b-col>
+        </b-row>
+        <Table_u></Table_u>
     </div>
 </template>
 
 <script>
     import Title from '@/components/Title.vue'
-    import Sidebar from '@/components/Sidebar.vue'
+    import Table_u from '@/components/Table_u.vue'
 
     export default {
         name: 'Admin',
         components: {
             Title,
-            Sidebar
+            Table_u
         }
     }
 </script>
