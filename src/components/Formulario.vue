9e64467f92d493922c56a177b8496388d73d6825
Orlando <xlandoxrojasx@gmail.com>
2020-07-04 15:18:24 -0500

    Se agrego el formulario de usuario en un complemento aparte asi como tambien la tabla.

---
+++ src/components/Formulario.vue
@@ -0,0 +1,100 @@
+<template>
+    <div>
+        <b-row>
+            <b-col cols="12">
+                <h3 class="mb-3">Registrar nuevo usuario:</h3>
+            </b-col>
+        </b-row>
+        <b-row class="justify-content-center">
+            <b-col sm="6">
+                <form>
+                    <label for="nom">Nombre</label>
+                    <input type="text" id="nom" class="form-control mb-2" placeholder="Nombre" v-model="usuario.nom">
+                    <label for="a_pat">Apellido Paterno</label>
+                    <input type="text" id="a_pat" class="form-control mb-2" placeholder="Apellido paterno" v-model="usuario.a_pat">
+                    <label for="a_mat">Apellido Materno</label>
+                    <input type="text" id="a_mat" class="form-control mb-2" placeholder="Apellido materno" v-model="usuario.a_mat">
+                    <label for="genero">Género</label>
+                    <select class="form-control mb-2" id="genero" v-model="usuario.genero">
+                        <option>Masculino</option>
+                        <option>Femenino</option>
+                    </select>
+                    <label for="f_nac">Fecha de nacimiento</label>
+                    <input type="date" id="f_nac" max="3000-12-31" min="1000-01-01" class="form-control mb-2" v-model="usuario.f_nac">
+                    <label for="alca">Alcaldía</label>
+                    <select class="form-control mb-2" id="alca" v-model="usuario.alca">
+                        <option>Álvaro Obregón</option>
+                        <option>Azcapotzalco</option>
+                        <option>Benito Juárez</option>
+                        <option>Coyoacán</option>
+                        <option>Cuajimalpa</option>
+                        <option>Cuauhtémoc</option>
+                        <option>Gustavo A. Madero</option>
+                        <option>Iztacalco</option>
+                        <option>Iztapalapa</option>
+                        <option>Magdalena Contreras</option>
+                        <option>Miguel Hidalgo</option>
+                        <option>Milpa Alta</option>
+                        <option>Tláhuac</option>
+                        <option>Tlalpan</option>
+                        <option>Venustiano Carranza</option>
+                        <option>Xochimilco</option>
+                    </select>
+                    <label for="col">Colonia</label>
+                    <input type="text" class="form-control mb-2" id="col" placeholder="Colonia" v-model="usuario.col">
+                    <label for="calle">Calle</label>
+                    <input type="text" class="form-control mb-2" id="calle" placeholder="Calle" v-model="usuario.calle">
+                    <label for="tel">Teléfono</label>
+                    <input type="number" class="form-control mb-2" id="tel" placeholder="Teléfono" v-model="usuario.tel">
+                    <label for="esc">Escolaridad</label>
+                    <select class="form-control mb-2" id="esc" v-model="usuario.esc">
+                        <option>Primaria</option>
+                        <option>Secundaria</option>
+                        <option>Preparatoria</option>
+                        <option>Universidad</option>
+                    </select>
+                    <label for="ocup">Ocupación</label>
+                    <input type="text" class="form-control mb-2" id="ocup" placeholder="Ocupación" v-model="usuario.ocup">
+                    <label for="correo">Correo</label>
+                    <input type="text" class="form-control mb-2" id="correo" placeholder="correo@dominio.com" v-model="usuario.correo">
+                    <label for="contra">Contraseña</label>
+                    <input type="password" class="form-control mb-2" id="contra" placeholder="Contraseña" v-model="usuario.contra">
+                    <label for="rol">Rol</label>
+                    <select class="form-control mb-3" id="rol" v-model="usuario.rol">
+                        <option>Usuario</option>
+                        <option>Exponente</option>
+                        <option>Administrador</option>
+                        <option>SuperUsuario</option>
+                    </select>
+                    <b-button variant="primary" block class="my-2" @click="alerta()">Agregar</b-button>
+                </form>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Formulario',
+        data() {
+            return {
+                usuario: {
+                    nom: '',
+                    a_pat: '',
+                    a_mat: '',
+                    genero: '',
+                    f_nac:  '',
+                    alca: '',
+                    col: '',
+                    calle: '',
+                    tel: '',
+                    esc: '',
+                    ocup: '',
+                    correo: '',
+                    contra: '',
+                    rol: ''
+                }
+            }
+        },
+    }
+</script>
--- src/components/Table_u.vue
+++ src/components/Table_u.vue
@@ -1,20 +1,11 @@
 <template>
     <div>
         <b-row>
-            <b-col cols="12">
-                <b-alert
-                    :show="dismissCountDown"
-                    dismissible
-                    variant="success"
-                    @dismissed="dismissCountDown=0"
-                    @dismiss-count-down="countDownChanged"
-                >
-                    {{mensaje.texto}}
-                </b-alert>
-                <h3>Colección usuarios:</h3>
+            <b-col>
+                <h3>Colección de usuarios:</h3>
             </b-col>
         </b-row>
-        <b-row>
+        <b-row class="mb-5">
             <b-col cols="12">
                 <table class="table">
                     <thead>
@@ -23,8 +14,8 @@
                             <th scope="col">Apellido P.</th>
                             <th scope="col">Apellido M.</th>
                             <th scope="col">Fecha de N.</th>
-                            <th scope="col">Alcaldia</th>
-                            <th scope="col">Telefono</th>
+                            <th scope="col">Alcaldía</th>
+                            <th scope="col">Teléfono</th>
                             <th scope="col">Correo</th>
                             <th scope="col">Acciones</th>
                         </tr>
@@ -38,65 +29,10 @@
                             <td>{{item.alca}}</td>
                             <td>{{item.tel}}</td>
                             <td>{{item.correo}}</td>
-                            <td><b-button variant="primary" @click="alerta()">Info</b-button></td>
+                            <td><b-button variant="primary">Info</b-button></td>
                         </tr>
                     </tbody>
                 </table>
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col>
-                <h3 class="mb-3">Registrar nuevo usuario:</h3>
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col>
-                <form>
-                    <label for="nom">Nombre</label>
-                    <input type="text" id="nom" class="form-control mb-2" placeholder="Nombre">
-                    <label for="a_pat">Apellido Paterno</label>
-                    <input type="text" id="a_pat" class="form-control mb-2" placeholder="Apellido paterno">
-                    <label for="a_mat">Apellido Materno</label>
-                    <input type="text" id="a_mat" class="form-control mb-2" placeholder="Apellido materno">
-                    <label for="genero">Genero</label>
-                    <select class="form-control mb-2" id="genero">
-                        <option>Masculino</option>
-                        <option>Femenino</option>
-                    </select>
-                    <label for="f_nac">Fecha de nacimiento</label>
-                    <input type="date" id="f_nac" max="3000-12-31" min="1000-01-01" class="form-control mb-2">
-                    <label for="alca">Alcaldía</label>
-                    <select class="form-control mb-2" id="alca">
-                        <option>Álvaro Obregón</option>
-                        <option>Azcapotzalco</option>
-                        <option>Benito Juárez</option>
-                        <option>Coyoacán</option>
-                        <option>Cuajimalpa</option>
-                        <option>Cuauhtémoc</option>
-                        <option>Gustavo A. Madero</option>
-                        <option>Iztacalco</option>
-                        <option>Iztapalapa</option>
-                        <option>Magdalena Contreras</option>
-                        <option>Miguel Hidalgo</option>
-                        <option>Milpa Alta</option>
-                        <option>Tláhuac</option>
-                        <option>Tlalpan</option>
-                        <option>Venustiano Carranza</option>
-                        <option>Xochimilco</option>
-                    </select>
-                    <label for="col">Colonia</label>
-                    <input type="text" class="form-control mb-2" id="col" placeholder="Colonia">
-                    <label for="calle">Calle</label>
-                    <input type="text" class="form-control mb-2" id="calle" placeholder="Calle">
-                    <label for="tel">Telefono</label>
-                    <input type="number" class="form-control mb-2" placeholder="Nombre">
-                    <input type="text" class="form-control mb-2" placeholder="Nombre">
-                    <input type="text" class="form-control mb-2" placeholder="Nombre">
-                    <input type="text" class="form-control mb-2" placeholder="Nombre">
-                    <input type="text" class="form-control mb-2" placeholder="Nombre">
-                    <input type="text" class="form-control mb-2" placeholder="Nombre">
-                    
-                </form>
             </b-col>
         </b-row>
     </div>
@@ -107,9 +43,6 @@
         name: 'Table_u',
         data() {
             return {
-                mensaje: {texto: ''},
-                dismissSecs: 5,
-                dismissCountDown: 0,
                 usuarios: []
             }
         },
@@ -117,11 +50,6 @@
             this.listarUsuarios();
         },
         methods: {
-            alerta(){
-                this.mensaje.color = 'success';
-                this.mensaje.texto = 'Probando alerta';
-                this.showAlert();
-            },
             listarUsuarios(){
                 this.axios.get('/usuarios').then(res => {
                     console.log(res.data);
@@ -129,12 +57,6 @@
                 }).catch(e => {
                     console.log(e.response);
                 })
-            },
-            countDownChanged(dismissCountDown) {
-                this.dismissCountDown = dismissCountDown
-            },
-            showAlert() {
-                this.dismissCountDown = this.dismissSecs
             }
         }
     }
